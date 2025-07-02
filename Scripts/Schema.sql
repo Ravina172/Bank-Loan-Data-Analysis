2ab045cda954b5e35f21a798d2d360ed73b0078b
Ravina172 <ravinapatidar13634@gmail.com>
2025-07-02 16:31:52 +0530

    Create Schema.sql

---
+++ Scripts/Schema.sql
@@ -0,0 +1,28 @@
+---- Schema for loan dataset
+DROP TABLE IF EXISTS financial_loan;
+CREATE TABLE financial_loan
+(
+id	
+address_state
+application_type	
+emp_length	
+emp_title	
+grade	
+home_ownership	
+issue_date	
+last_credit_pull_date	
+last_payment_date	
+loan_status	
+next_payment_date	
+member_id	
+purpose	
+sub_grade	
+term	
+verification_status	
+annual_income	
+dti	
+installment	
+int_rate	
+loan_amount	
+total_acc	total_payment
+  );
